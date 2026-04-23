5b4a22fc3e72cc0dc1c9de3abcaee1d6f2e101d1
GiuNogueiraSa <giunogueiralopes@gmail.com>
2026-04-23 20:51:39 -0300

    Commit desafio xp

---
+++ desafio.py
@@ -0,0 +1,46 @@
+import re
+
+def ehEmailValido(email):
+    padrao = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return bool(re.match(padrao, email))
+
+def _calcular_digito_cpf(cpf_parcial, peso_inicial):
+    soma = sum(int(cpf_parcial[i]) * (peso_inicial - i) for i in range(len(cpf_parcial)))
+    return (soma * 10 % 11) % 10
+
+def ehCPFValido(cpf):
+    cpf = re.sub(r'\D', '', str(cpf))
+    
+    if len(cpf) != 11 or cpf == cpf[0] * 11:
+        return False
+    
+    digito_1 = _calcular_digito_cpf(cpf[:9], 10)
+    digito_2 = _calcular_digito_cpf(cpf[:10], 11)
+    
+    return int(cpf[9]) == digito_1 and int(cpf[10]) == digito_2
+
+def ehSenhaForte(senha):
+    if len(senha) < 8: return False
+    tem_maiuscula = any(c.isupper() for c in senha)
+    tem_numero = any(c.isdigit() for c in senha)
+    tem_simbolo = any(not c.isalnum() for c in senha)
+    return tem_maiuscula and tem_numero and tem_simbolo
+
+def calcular_pontos(gols_real, gols_palpite):
+    """
+    Calcula pontos do bolão:
+    - 25 pts: Placar Exato
+    - 10 pts: Acertou vencedor/empate mas errou placar
+    - 0 pts: Errou tudo
+    """
+    if gols_real == gols_palpite:
+        return 25
+    
+    # Verifica tendência (Vitória Casa, Empate, Vitória Fora)
+    tendencia_real = (gols_real[0] > gols_real[1]) - (gols_real[0] < gols_real[1])
+    tendencia_palpite = (gols_palpite[0] > gols_palpite[1]) - (gols_palpite[0] < gols_palpite[1])
+    
+    if tendencia_real == tendencia_palpite:
+        return 10
+        
+    return 0
---
+++ test_desafio.py
@@ -0,0 +1,40 @@
+import unittest
+from desafio import ehEmailValido, ehCPFValido, ehSenhaForte, calcular_pontos
+
+class TestDesafioTDD(unittest.TestCase):
+    # Caso 1: Email válido
+    def test_email_valido(self):
+        self.assertTrue(ehEmailValido("contato@giuliamarco.com.br"))
+
+    # Caso 2: Email inválido (sem @)
+    def test_email_sem_arroba(self):
+        self.assertFalse(ehEmailValido("contatogiuliamarco.com.br"))
+
+    # Caso 3: CPF válido (Algoritmo oficial)
+    def test_cpf_valido(self):
+        self.assertTrue(ehCPFValido("12345678909"))
+
+    # Caso 4: CPF inválido (Números repetidos)
+    def test_cpf_repetido(self):
+        self.assertFalse(ehCPFValido("00000000000"))
+
+    # Caso 5: Senha forte (Mínimo 8 caracteres, letra maiúscula, número e símbolo)
+    def test_senha_forte(self):
+        self.assertTrue(ehSenhaForte("Brasil@2026"))
+
+class TestBolaoXP(unittest.TestCase):
+
+    def test_placar_exato(self):
+        self.assertEqual(calcular_pontos((2, 1), (2, 1)), 25)
+
+    def test_vencedor_correto_placar_errado(self):
+        self.assertEqual(calcular_pontos((2, 1), (3, 0)), 10)
+
+    def test_empate_correto_placar_errado(self):
+        self.assertEqual(calcular_pontos((1, 1), (0, 0)), 10)
+
+    def test_erro_total(self):
+        self.assertEqual(calcular_pontos((2, 1), (0, 2)), 0)
+
+if __name__ == '__main__':
+    unittest.main()
